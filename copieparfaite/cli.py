dc94c4cc5c2fc56cbce2a44d7898b02fffc78299
Emmanuel Ch <96680649+emmanuel-ch@users.noreply.github.com>
2023-02-08 18:46:14 +0100

    Pass 2 options in command.

--- copieparfaite/cli.py
+++ copieparfaite/cli.py
@@ -7,7 +7,10 @@ from copieparfaite import __appname__, __version__, view, working_session
 
 
 @app.command()
-def main() -> None:
+def main(
+    auto_copy: bool = typer.Option(True, '--auto_copy', '-a', help='Auto-copy files that aren\'t mirrored', rich_help_panel='Mirroring options'),
+    solve_conflicts_by_suffixing = type.Option(True, '--suffix_conflicts', '-s', help='Append suffix upon conflicts (Only activated works)', rich_help_panel='Mirroring options')
+) -> None:
     console = view.ConsoleManager()
     console.show_welcome_message()
     
@@ -30,10 +33,7 @@ def main() -> None:
     ws.unified_filetree = ws.make_tree(ws.dirA_path, ws.dirB_path)
     view.print_filetree(ws.unified_filetree, diff_only=True)
     
-    # Auto-copy?
-    auto_copy = view.YN_question('Do you want to automatically copy files which aren\'t mirrored?', 'Y')
-    solve_all_conflicts_by_AB_suffix = True
-    ws.run_synchronizer(auto_copy, solve_all_conflicts_by_AB_suffix)
+    ws.run_synchronizer(auto_copy, solve_conflicts_by_suffixing)
     view.print_msg('[i] Synchronization finished.')
     
     return None
